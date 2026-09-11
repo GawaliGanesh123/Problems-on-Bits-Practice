354130bc14339c5dc053b5aed2c404faaba79fbd
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-09-11 09:56:51 +0530

    Add files via upload

---
+++ 02_Check_Second_Bit.cpp
@@ -0,0 +1,53 @@
+/* Accept number from user and check whether second bit of that number is on or off.
+
+Input- 10
+Output- Bit is ON
+
+Input- 8
+Output- Bit is OFF
+
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include<iostream>
+using namespace std;
+
+bool CheckBit(int iNo)
+{
+    int iMask = 0x2;
+    int iResult = 0;
+    iResult = iNo & iMask;
+
+    if(iResult == iMask)
+    {   
+      return true;   
+    }
+    else
+    {   
+     return false;    
+    }
+}
+
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    cout<<"Enter number : \n";
+    cin>>iValue;
+
+    bRet = CheckBit(iValue);
+
+    if(bRet == true)
+    {
+        cout<<"Bit is ON\n";
+    }
+    else
+    {
+        cout<<"Bit is OFF\n";
+    }
+    
+    return 0;
+}
