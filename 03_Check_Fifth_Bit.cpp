1651fde303dbba74986d90ba97e08f9e9f0ecb2b
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-09-11 09:57:09 +0530

    Add files via upload

---
+++ 03_Check_Fifth_Bit.cpp
@@ -0,0 +1,55 @@
+/* Take a number from user and check whether fifth bit of that number is on or off.
+
+Input- 20
+Output- Bit is ON
+
+Input- 10
+Output- Bit is OFF
+
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include<iostream>
+using namespace std;
+
+bool CheckBit(int iNo)
+{
+    int iMask = 0x10;
+    int iResult = 0;
+    iResult = iNo & iMask;
+
+    if(iResult == iMask)
+    {   
+        return true;   
+    }
+    else
+    {   
+        return false;    
+    }
+}
+
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    cout<<"Enter number : \n";
+    cin>>iValue;
+
+    bRet = CheckBit(iValue);
+
+    if(bRet == true)
+    {
+        cout<<"Bit is ON\n";
+    }
+    else
+    {
+        cout<<"Bit is OFF\n";
+    }
+    
+    return 0;
+}
+
+
