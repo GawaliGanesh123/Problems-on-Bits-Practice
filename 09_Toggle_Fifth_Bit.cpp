75aad6e17a907d8456567d3743be1eeb332b2fb0
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-09-14 10:24:04 +0530

    Add files via upload

---
+++ 09_Toggle_Fifth_Bit.cpp
@@ -0,0 +1,40 @@
+/* Accept number from user and toggle the fifth bit of that number.
+
+Input- 25
+Output- Number after bit toggle : 9
+
+Input- 9
+Output- Number after bit toggle : 25
+
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include<iostream>
+using namespace std;
+
+int ToggleBit(int iNo)
+{
+    int iMask = 0x10;
+    int iResult = 0;
+    iResult = iNo ^ iMask;
+    return iResult;
+}
+
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    cout<<"Enter the number : \n";
+    cin>>iValue;
+
+    iRet = ToggleBit(iValue);
+
+    cout<<"Number after bit toggle : "<<iRet<<"\n";
+    
+    return 0;
+}
+
+
