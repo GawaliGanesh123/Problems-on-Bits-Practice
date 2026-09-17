0936e5f1f1166efbce6bd03314a9be20f340746e
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-09-17 09:45:36 +0530

    Add files via upload

---
+++ 13_Bitwise_AND.cpp
@@ -0,0 +1,34 @@
+/* Accept two numbers from user and perform bitwise AND operation.
+
+Input- 12
+       10
+Output- Bitwise AND : 8
+
+Input- 15
+        7
+Output- Bitwise AND : 7
+
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+    int iResult = 0;
+
+    cout<<"Enter first number : \n";
+    cin>>iValue1;
+
+    cout<<"Enter second number : \n";
+    cin>>iValue2;
+
+    iResult = iValue1 & iValue2;
+
+    cout<<"Bitwise AND : "<<iResult<<"\n";
+    
+    return 0;
+}
