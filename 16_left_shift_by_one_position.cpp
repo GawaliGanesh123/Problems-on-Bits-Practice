d00c7b20376de9afaec1b385c2891661180b49a5
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-09-21 10:44:45 +0530

    Add files via upload

---
+++ 16_left_shift_by_one_position.cpp
@@ -0,0 +1,29 @@
+/* Accept a number from user and perform left shift operation by one position.
+
+Input- 10
+Output- Left Shift : 20
+
+Input- 7
+Output- Left Shift : 14
+
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iValue = 0;
+    int iResult = 0;
+
+    cout<<"Enter the number : \n";
+    cin>>iValue;
+
+    iResult = iValue << 1;
+
+    cout<<"Left Shift : "<<iResult<<"\n";
+    
+    return 0;
+}
