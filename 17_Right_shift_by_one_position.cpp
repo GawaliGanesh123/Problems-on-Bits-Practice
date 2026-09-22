ce7f56bf4d19e19bd68c101c878416abafcd17ba
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2026-09-22 10:36:23 +0530

    Add files via upload

---
+++ 17_Right_shift_by_one_position.cpp
@@ -0,0 +1,29 @@
+/* Accept a number from user and perform right shift operation by one position.
+
+Input- 10
+Output- Right Shift : 5
+
+Input- 8
+Output- Right Shift : 4
+
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iValue = 0;
+    int iResult = 0;
+
+    cout<<"Enter the number : \n";
+    cin>>iValue;
+
+    iResult = iValue >> 1;
+
+    cout<<"Right Shift : "<<iResult<<"\n";
+    
+    return 0;
+}
